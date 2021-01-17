67c83af999952b6aedbec23650e771c00c40536e
palacionis <jonas.palacionis@gmail.comm>
2021-01-17 19:58:55 +0200

    adding 1st chapter - introduction to algorithms

--- Chapter 1/intoduction_to_algorithms.py
+++ Chapter 1/intoduction_to_algorithms.py
@@ -1,1 +1,36 @@
 # Chapter 1 - introduction to algorithms
+
+# For any list of n, binary search will take log2n steps to run the worst case. 2 is subscript
+# whereas simple search will take n
+
+# binary search only works when the list is sorted
+
+def binary_search(list, item):
+  low = 0
+  high = len(list)-1
+
+  while low <= high:
+    mid = (low + high) // 2
+    guess = list[mid]
+
+    if guess == item:
+      return mid
+
+    if guess > item:
+      high = mid - 1
+    
+    else:
+      low = mid + 1
+
+  return None
+
+
+# some bigO notations from fastest to slowest:
+
+# O(logn) - log time: example - binary search
+# O(n) - linear time: example - simple search
+# O(n * logn) example - quicksort
+# O(n2) - 2 is superscript - a slow sorting algorithm, example: selection sort
+# O(n!) - a really slow algorithm: example - traveling salesperson
+
+# algorithm times are measured in terms of growth of an algorithm
